0873e8e1fce51731842084f67dfabee5626e9a8e
Jose Chavira <joeychavira2016@gmail.com>
2019-08-28 08:17:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@ def find_max_value(array)
   max = 0
     array.length.times do |index|
       if array|index| > max
-        max = array[count]
+        max = array[index]
       end
     end
     max
